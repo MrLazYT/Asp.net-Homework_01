41410b533aefb11047d3bdf5515c005467436ec3
mrla3yt <gladandenys@gmail.com>
2024-08-25 16:41:58 +0300

    Complete task_02

---
+++ project_1/Pages/AboutMe.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace project_1.Pages
+{
+    public class AboutMeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
